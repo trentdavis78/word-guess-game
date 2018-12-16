f14a86a0a688c002891dcaf3000799fc4c366fb8
Trent Davis <trentdavisinc@gmail.com>
2018-12-15 23:24:38 -0600

    Header Layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,39 @@
+//variables = solution array, win total, miss total
+var DOMGame = document.getElementById('game');
+var DOMSplash = document.getElementById('splash');
+var DOMScore = document.getElementById('score');
+var DOMMiss = document.getElementById('miss');
+var DOMHint = document.getElementById('hint');
 
+var score = 0;
+var miss = 0;
+var hint = 3;
+
+
+// Splash page w/ 'press any key to continue' event listener
+document.onkeydown = function(e) {
+    DOMSplash.classList.add("hidden");
+    DOMGame.classList.remove("opacity-1");
+    DOMGame.classList.add("opacity-f");
+    start();
+}
+
+// reset game to defaults and start game
+function start() {
+    DOMHint.innerHTML = hint;
+}
+
+// onkeypress event function --> check if letter pressed is in the solution,
+// if it is, show letter, if not increment miss total
+
+// check if miss total = 6, if so end game
+// check if solved, if so end game
+// else fire onkeypress event function again
+
+//game reset function
+function reset() {
+    score = 0;
+    miss = 0;
+    hint = 3;
+    DOMHint.innerHTML = hint;
+}
